04e5ecbb0041551dbfb8a8659f6600c3c0d720da
enaml <monotonalist@gmail.com>
2015-05-19 17:12:31 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'svDyuAJtV0HvyYeceXhS4Znqn',
+    consumer_secret: 'Gm8xqLTTIXnV98kuMebBPb97uOMSfqp8ucNiDf4aAzFHzjgd26',
+    access_token_key: '45786550-sQAwPFx1UEpc1pGG4jq3LzBtwP7WO4FHVAA5KKnnJ',
+    access_token_secret: 'dAKtCri5eSscGzBSltDOZtk9XeEkFxv3j04Wjx4yzkuhF'
+  }
+}
